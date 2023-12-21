7cbb3029b9312fc063021969f0001fee3343d2c8
Joy Kumar Acharjee <joy29940@gmail.com>
2023-12-21 06:49:18 +0600

    add login desing

--- src/main/java/com/retake/stuaid/controller/LoginFormController.java
+++ src/main/java/com/retake/stuaid/controller/LoginFormController.java
@@ -1,4 +1,9 @@
 package com.retake.stuaid.controller;
 
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public PasswordField txtPass;
+    public AnchorPane txtEmail;
 }
