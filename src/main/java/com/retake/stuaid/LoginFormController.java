4a5f739338c269e737641cf2b315acdfbc1353ed
akms-limon <akmslimon@gmail.com>
2023-12-21 23:06:42 +0600

    Update Various Scene change button

--- src/main/java/com/retake/stuaid/LoginFormController.java
+++ src/main/java/com/retake/stuaid/LoginFormController.java
@@ -7,7 +7,6 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -33,32 +32,11 @@ public class LoginFormController {
     @FXML
     private PasswordField txtPass;
 
-//    @FXML
-//    void initialize() {
-//        txtCreateAcc.setOnAction(event -> {
-//            txtCreateAcc.getScene().getWindow().hide();
-//            FXMLLoader loader = new FXMLLoader();
-//            loader.setLocation(getClass().getResource("../SignupForm.fxml"));
-//            try {
-//                loader.load();
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
-//            Parent root = loader.getRoot();
-//            Stage stage = new Stage();
-//            stage.setTitle("Sign Up");
-//            stage.setScene(new Scene(root));
-//            stage.setResizable(false);
-//            stage.showAndWait();
-//        });
-//    }
-
     public void createNewAccnt(ActionEvent actionEvent) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("SignupForm.fxml"));
-        Scene scene = new Scene(loader.load());
-        Stage crntStage = (Stage) root.getScene().getWindow();
-        crntStage.setScene(scene);
-        crntStage.setTitle("Sign Up");
-        crntStage.centerOnScreen();
+        SceneChangerUtility.changeScene(root, "SignupForm.fxml", "Sign Up");
+    }
+
+    public void gotoHomePage(ActionEvent actionEvent) throws IOException {
+        SceneChangerUtility.changeScene(root, "HomePage.fxml", "Home Page");
     }
 }
--- src/main/java/com/retake/stuaid/SignupController.java
+++ src/main/java/com/retake/stuaid/SignupController.java
@@ -1,4 +1,59 @@
 package com.retake.stuaid;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 public class SignupController {
+
+    public Button txtSignUp;
+    @FXML
+    private BorderPane root;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private CheckBox chkboxTeacher;
+
+    @FXML
+    private Button txtSignIn;
+
+    @FXML
+    private PasswordField txtConfirmPass;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private PasswordField txtPass;
+
+//    @FXML
+//    void initialize() {
+//
+//    }
+    public void gotoLoginPagebyClickingLogIn(ActionEvent actionEvent) throws IOException {
+        SceneChangerUtility.changeScene(root, "LoginForm.fxml", "Log In");
+    }
+
+    public void gotoLoginPagebyClickingSignUp(ActionEvent actionEvent) throws IOException {
+        SceneChangerUtility.changeScene(root, "LoginForm.fxml", "Log In");
+    }
 }
