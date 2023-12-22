cd9bb6759caceb3c3eb433d01e72ba4bbfbd431b
Risan Mahfuz <risanmahfuz2838@gmail.com>
2023-12-23 02:12:14 +0600

    Database signup

--- src/main/java/com/retake/stuaid/database/DatabaseHandler.java
+++ src/main/java/com/retake/stuaid/database/DatabaseHandler.java
@@ -1,6 +1,7 @@
 package com.retake.stuaid.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseHandler extends Configs {
@@ -9,5 +10,20 @@ public class DatabaseHandler extends Configs {
         String connectionString = "jdbc:postgresql://" + dbHost + ":" + dbPort + "/" + dbName + "?" + "sslmode=verify-full";
         dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPassword);
         return dbConnection;
+    }
+    public void signupUser(String email,String name,String password,char usertype){
+        String insert="INSERT INTO "+"projectuser "+"(email, name, password, usertype) "+
+                "VALUES (?,?,?,?)";
+
+        try {
+            PreparedStatement preparedStatement=getDbConnection().prepareStatement(insert);
+            preparedStatement.setString(1,email);
+            preparedStatement.setString(2,name);
+            preparedStatement.setString(3,password);
+            preparedStatement.setString(4, toString().valueOf(usertype));
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
