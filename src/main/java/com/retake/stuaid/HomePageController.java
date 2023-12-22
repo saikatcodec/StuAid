d1db7e949e1c2d9a57bf9199f4862258fbfee082
akms-limon <akmslimon@gmail.com>
2023-12-22 21:51:24 +0600

    Major updated

--- src/main/java/com/retake/stuaid/HomePageController.java
+++ src/main/java/com/retake/stuaid/HomePageController.java
@@ -1,4 +1,30 @@
 package com.retake.stuaid;
 
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
 public class HomePageController {
+
+    @FXML
+    private VBox root;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnLogout;
+
+    @FXML
+    private Button btnProfile;
+
+    @FXML
+    private void gotoLoginPageByClickingLogout(ActionEvent actionEvent) throws IOException {
+        SceneChangerUtility.changeScene(root, "LoginForm.fxml", "Log In");
+    }
+
 }
