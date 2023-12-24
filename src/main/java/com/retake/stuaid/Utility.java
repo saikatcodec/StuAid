888714518315d584d2111e8a2b818a1d2bcf4790
akms-limon <akmslimon@gmail.com>
2023-12-24 06:19:23 +0600

    Add pop-up action in homepage

--- src/main/java/com/retake/stuaid/Utility.java
+++ src/main/java/com/retake/stuaid/Utility.java
@@ -7,13 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class SceneChangerUtility {
+public class Utility {
     public static void changeScene(Parent root, String name, String title) throws IOException {
-        FXMLLoader loader = new FXMLLoader(SceneChangerUtility.class.getResource(name));
+        FXMLLoader loader = new FXMLLoader(Utility.class.getResource(name));
         Scene scene = new Scene(loader.load());
         Stage crntStage = (Stage) root.getScene().getWindow();
         crntStage.setScene(scene);
         crntStage.setTitle(title);
         crntStage.centerOnScreen();
+    }
+
+    public static void popUp() {
+
     }
 }
