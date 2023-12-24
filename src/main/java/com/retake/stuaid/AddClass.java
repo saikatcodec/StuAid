139b4ee86d44b528e340b0c6df0c4d9854903b9b
Joy Kumar Acharjee <joy29940@gmail.com>
2023-12-24 16:49:28 +0600

    Login build for class submit

--- src/main/java/com/retake/stuaid/AddClass.java
+++ src/main/java/com/retake/stuaid/AddClass.java
@@ -1,14 +1,20 @@
 package com.retake.stuaid;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import java.util.SimpleTimeZone;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.ResourceBundle;
 
 public class AddClass {
 
@@ -41,6 +47,7 @@ public class AddClass {
 
     @FXML
     private TextField txtTimeMin;
+    private final String color = "#B80000";
 
     @FXML
     ObservableList<String> AmPmlist = FXCollections.observableArrayList("AM", "PM");
@@ -57,8 +64,40 @@ public class AddClass {
     }
 
     @FXML
-    private void addNewCourse(ActionEvent event) {
+    private void addNewCourse(ActionEvent event) throws ParseException {
+        String classTitle = txtCourseTitle.getText();
+        String dateOf = choiceTdayTmrow.getValue();
+        LocalDate date = LocalDate.now();
+        String hour = txtTimeHr.getText();
+        String minute = txtTimeMin.getText();
+        String amOrPm = choiceAmPm.getValue();
+        boolean flag = true;
 
+        if (classTitle.isBlank()) {
+            setBorderColor(txtCourseTitle, color);
+            flag = false;
+        } else {
+            setBorderColor(txtCourseTitle, "transparent");
+        }
+
+        if (hour.isBlank() || minute.isBlank()) {
+            setBorderColor(txtTimeHr, color);
+            setBorderColor(txtTimeMin, color);
+            flag = false;
+        } else {
+            setBorderColor(txtTimeHr, "transparent");
+            setBorderColor(txtTimeMin, "transparent");
+        }
+
+        if (flag) {
+            if (dateOf.equals("Tomorrow")) {
+                date = date.plusDays(1);
+            }
+
+            String timeString = hour + ":" + minute + " " + amOrPm;
+            SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm aa");
+            Date time = timeFormat.parse(timeString);
+        }
     }
 
     @FXML
@@ -66,4 +105,13 @@ public class AddClass {
 
     }
 
+    /**
+     * Set border of Node
+     *
+     * @param node  Node of textField
+     * @param color border color
+     */
+    private void setBorderColor(TextField node, String color) {
+        node.setStyle("-fx-border-color: " + color);
+    }
 }
