546c6265aa72e33fd2a56f60b3674cc5b7dc2960
Joy Kumar Acharjee <joy29940@gmail.com>
2023-12-25 06:47:22 +0600

    Fix the refresh button

--- src/main/java/com/retake/stuaid/HomePageController.java
+++ src/main/java/com/retake/stuaid/HomePageController.java
@@ -17,35 +17,43 @@ import java.time.LocalDate;
 
 public class HomePageController {
     @FXML
+    private Button btnRefresh;
+    @FXML
+    private Button btnAddCT;
+    @FXML
+    private Button btnAddAssignment;
+    @FXML
+    private Button btnAddRef;
+    @FXML
+    private Button btnAddNotice;
+    @FXML
     private Button btnToday;
-
     @FXML
     private VBox root;
-
     @FXML
     private URL location;
-
     @FXML
     private Button btnLogout;
-
     @FXML
     private Button btnProfile;
-
     @FXML
     private VBox vTaskItems;
-
     @FXML
     private VBox vUpcmngTaskItems;
-
     @FXML
     private VBox vCT;
-
     @FXML
     private VBox vAssignment;
 
     DatabaseHandler handler = new DatabaseHandler();
 
     public void updateHomeDisplay() throws SQLException, IOException {
+        vTaskItems.getChildren().clear();
+        vUpcmngTaskItems.getChildren().clear();
+        vCT.getChildren().clear();
+        vCT.getChildren().add(btnAddCT);
+        vAssignment.getChildren().clear();
+        vAssignment.getChildren().add(btnAddAssignment);
 
         LocalDate today = LocalDate.now();
         handler.DeletePreviousTasks(today);
